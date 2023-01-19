6546109e8e724825f221d6b379116da295ad0535
Kendra Odrunia <kendra.odru@gmail.com>
2023-01-18 17:50:05 -0800

    add dep

---
+++ server.js
@@ -0,0 +1,11 @@
+console.log("server is running...")
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome!');
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is listening on port ${port}...`));
