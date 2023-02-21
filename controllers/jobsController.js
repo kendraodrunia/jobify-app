24997452e5ee815ee2f947e39853a7aa14717e0f
Kendra Odrunia <kendra.odru@gmail.com>
2023-02-20 17:43:41 -0800

    add job controller

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,20 @@
-const createJob = async(req,res) =>{
-    res.send('create job')
-}
+import Job from '../models/Job.js';
+import { StatusCodes } from 'http-status-codes';
+import { BadRequestError, NotFoundError } from '../errors/index.js';
+
+const createJob = async (req, res) => {
+  const { position, company } = req.body;
+
+  if (!position || !company) {
+    throw new BadRequestError('Please Provide All Values');
+  }
+
+  req.body.createdBy = req.user.userID;
+
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
+};
+
 const getAllJobs = async(req,res) =>{
     res.send('get all jobs')
 }
